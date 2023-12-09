ad9969e748f4768d2ba4aace9d6a4b724b44cfc4
Vladimir Varbanov <vladimir.varbanov@puzzel.com>
2023-12-09 12:42:44 +0200

    test: unit tests adjustments

--- test/helper.js
+++ test/helper.js
@@ -1,10 +1,10 @@
-import Aurelia, { CustomElement } from 'aurelia';
+import Aurelia, {CustomElement} from 'aurelia';
 export async function render(template, ...deps) {
   const wrapper = CustomElement.define({name: 'wrapper', template});
   const div = document.createElement('div');
   const au = Aurelia.register(deps).app({
     host: div,
-    component: wrapper
+    component: wrapper,
   });
   await au.start();
   return div;
