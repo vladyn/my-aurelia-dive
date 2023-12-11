077145590015c5d0a4912a7ae6b237e2f70d8ea8
Vladimir Varbanov <vlado@vlado1.com>
2023-12-11 22:21:43 +0200

    refactor: refactoring various stles, mocks and constants

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,11 +1,43 @@
 import {newInstanceOf, resolve} from '@aurelia/kernel';
 import {IHttpClient} from '@aurelia/fetch-client';
+import {ENDPOINTS} from '../constants/endpoints';
 
 export class HttpService {
   httpClient = resolve(newInstanceOf(IHttpClient));
 
+  // Wrap up a fetch call
   async get(url) {
     return await this.httpClient.fetch(url);
+  }
+
+  async getUnits() {
+    try {
+      const response = await this.httpClient.fetch(ENDPOINTS.units);
+      return await response.json();
+    } catch (error) {
+      console.error('error fetching units', error);
+      throw error;
+    }
+  }
+
+  async getCases() {
+    try {
+      const response = await this.httpClient.fetch(ENDPOINTS.cases);
+      return await response.json();
+    } catch (error) {
+      console.error('error fetching cases', error);
+      throw error;
+    }
+  }
+
+  async getAddresses() {
+    try {
+      const response = await this.httpClient.fetch(ENDPOINTS.addresses);
+      return await response.json();
+    } catch (error) {
+      console.error('error fetching addresses', error);
+      throw error;
+    }
   }
 
   async post(url, body) {
