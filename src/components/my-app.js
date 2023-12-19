b020a9b23ce231b08a6328c9c2096e764156b944
Vladimir Varbanov <vlado@vlado1.com>
2023-12-19 15:51:38 +0200

    Removing redundant debug code refactor: removing the debugging code

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -1,13 +1,8 @@
-import {MyDialog} from './my-dialog';
 import {Cases} from './cases';
 import {Addresses} from './addresses';
 import {Home} from './home';
-import {fromState} from '@aurelia/state';
-import {IDialogService} from '@aurelia/dialog';
-import {resolve, newInstanceOf} from '@aurelia/kernel';
 
 export class MyApp {
-    static inject = [IDialogService];
     static routes = [
         {
             path: '',
@@ -35,30 +30,4 @@ export class MyApp {
             title: 'Case Details',
         },
     ];
-    message = 'Hello World!';
-    dialog = resolve(newInstanceOf(MyDialog));
-    @fromState((state) => state.keywords)
-        keywords;
-
-    constructor(dialogService) {
-        this.message = 'Hello World!';
-        this.dialogService = dialogService;
-    }
-
-    clickHandler() {
-        this.message = 'Hello Aurelia!';
-    }
-
-    showDialog() {
-        this.dialogService
-            .open({component: () => MyDialog, model: {}})
-            .then((openDialogResult) => {
-                // Note:
-                // We get here when the dialog is opened,
-                // and we are able to close dialog
-                setTimeout(() => {
-                    openDialogResult.dialog.cancel('Failed to finish editing after 3 seconds');
-                }, 3000);
-            });
-    }
 }
